54170282185f91ff6863a771ec0bd135c6b2e530
toritori3165 <miurasaho1988@gmail.com>
2024-01-29 12:29:36 +0900

    ビューの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,14 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
-  validates_format_of :encrypted_password, with: /\A[a-zA-Z0-9]+\z/
-  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/u
-  validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー]+\z/
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
