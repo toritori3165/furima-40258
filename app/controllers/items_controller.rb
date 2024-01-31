2b6657542f806cfb3d1a1189c8f2303226ef5cbc
toritori3165 <miurasaho1988@gmail.com>
2024-01-31 21:38:15 +0900

    アクション定義、ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,7 +20,21 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -27,4 +42,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :shipping_fee_id, :area_id, :shipping_to_date_id, :price, :image ).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end  
 end
